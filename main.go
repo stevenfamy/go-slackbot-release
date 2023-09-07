e303e94c61584e2fc8e05c2bfd1c0fcd1e94fb94
Steven Famy <stevenfamy@proton.me>
2023-09-07 12:08:48 +0700

    feat: add debug msg

--- main.go
+++ main.go
@@ -558,24 +558,24 @@ func handleStatusCommand(command slack.SlashCommand, client *slack.Client) error
 	// Create the attachment and assigned based on the message
 	attachment := slack.Attachment{}
 	// Add Some default context like user who mentioned the bot
-	attachment.Fields = []slack.AttachmentField{
-		{
-			Title: "Date",
-			Value: time.Now().String(),
-		}, {
-			Title: "Initializer",
-			Value: command.UserName,
-		},
+	attachment.Fields = []slack.AttachmentField{}
+
+	result := models.GetServerStatus("logistics")
+	user, err := client.GetUserInfo(command.UserName)
+	if err != nil {
+		return err
 	}
 
-	// Greet the user
-	attachment.Text = fmt.Sprintf("Hello %s", command.Text)
-	attachment.Color = "#4af030"
+	if result != "" {
+		attachment.Text = fmt.Sprintf("Gotcha <@%s>, this is the Testing Server Status for project %s: \n\n %s", user.ID, "logistics", result)
+	} else {
+		attachment.Text = fmt.Sprintf("Sorry <@%s>, Testing Server Status not found.", user.ID)
+	}
 
 	// Send the message to the channel
 	// The Channel is available in the command.ChannelID
-	_, _, err := client.PostMessage(command.ChannelID, slack.MsgOptionAttachments(attachment))
-	if err != nil {
+	_, _, err2 := client.PostMessage(command.ChannelID, slack.MsgOptionAttachments(attachment))
+	if err2 != nil {
 		return fmt.Errorf("failed to post message: %w", err)
 	}
 	return nil
